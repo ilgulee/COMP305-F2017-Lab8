a9e6c42873424d0164ecaa3f7e7b30a883f1e8bd
ilgulee <iglee2000ca@gmail.com>
2017-12-22 04:03:53 -0500

    Initial commit

---
+++ Assets/AiController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets
+{
+    public class AiController : MonoBehaviour 
+    {
+
+        public GameObject Goal;
+
+        private NavMeshAgent _agent;
+         
+        // Use this for initialization
+        void Start ()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+            _agent.SetDestination(Goal.transform.position);
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+    }
+}
---
+++ Assets/RaycasterMove.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class RaycasterMove : MonoBehaviour
+    {
+
+        public Camera Camera;
+        private Ray _ray;
+        private RaycastHit _hit;
+        private GameObject _hitGameObject = null;
+
+        void Update()
+        {
+            //Get object
+            if (Input.GetMouseButtonDown(0))
+            {
+                _ray = Camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(_ray, out _hit))
+                {
+                    if (_hit.collider.tag== "MoveableObject")
+                    {
+                        _hitGameObject = _hit.collider.gameObject;
+                    }
+                }
+            }
+
+            if (_hitGameObject)
+            {
+                //Hold object
+                if (Input.GetMouseButton(0))
+                {
+                    _ray = Camera.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(_ray, out _hit))
+                    {
+                        _hitGameObject.transform.position=new Vector3(_hit.point.x,_hitGameObject.transform.position.y,_hit.point.z);
+                    }
+                }
+                //Drop object
+                if (Input.GetMouseButtonUp(0))
+                {
+                    _hitGameObject = null;
+                }
+            }
+
+
+
+        }
+
+    }
+}
